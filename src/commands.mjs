0621d546d0fbd20dbc11c6eb28be0f06107ef7db
Gustaf Svensson <gussvens@kth.se>
2024-10-12 15:12:25 +0200

    Added an easter egg :)

--- src/commands.mjs
+++ src/commands.mjs
@@ -2,7 +2,17 @@ export const registerCommands = (app) => {
     app.command('/hi', async ({ command, ack, say }) => {
         try {
             await ack();
-            await say(`Hello!`);
+            await say(`Hello <@${command.user_id}>!`);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+
+    app.command('/thx1138', async ({ command, ack, say }) => {
+        try {
+            await ack();
+            await say(`The force is strong with this one.`);
         }
         catch (error) {
             console.error(error);
